bd566d38799990042f8921eb0e88fe55eadb0db8
Cricri Au <andcricri93@gmail.com>
2023-11-27 10:31:04 +0100

    quest 01.1 TDD

--- index.js
+++ index.js
@@ -1,65 +1,11 @@
-const express = require("express");
-
-const app = express();
+const app = require('./app');
 
 const port = 5000;
 
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
-app.get("/",(req,res) => {
-  res.send("Welcome to my favourite movie list");
-});
-
-const getMovies = (req,res) => {
-  res.status(200).json(movies);
-};
-
-app.get("/api/movies", getMovies);
-
-
-app.get("/api/movies/:id", (req,res) => {
-  const getIdMovie = parseInt(req.params.id)
-  const movie = movies.find (
-    (movie) => movie.id === getIdMovie
-    );
-
-  if (movie != null){
-    res.status(200).json(movie);
-  } else {
-    res.status(404).send("Not found");
-  }
-});
-
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
+app
+  .listen(port, () => {
+    console.info(`Server is listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error:", err.message);
+  });
